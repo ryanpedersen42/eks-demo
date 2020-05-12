6c56a1e8d62089dc859766f2dbfbed47ef2ef787
Ryan Pedersen <ryanpedersen@Ryans-MBP-2.attlocal.net>
2020-05-12 15:17:47 -0500

    exit 0

---
+++ .circleci/__tests__/test.js
@@ -0,0 +1,7 @@
+describe("Sum numbers", function() {
+    it("should sum two numbers correctly", function() {
+      var sum = 1 + 2;
+      var expectedResult = 3;
+      expect(sum).toEqual(expectedResult);
+    });
+  });
